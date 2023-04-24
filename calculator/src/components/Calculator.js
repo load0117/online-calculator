bd16d9b59fdd6fd70df72ca8a5bf44748f82dffc
HK <load0117@gmail.com>
2023-04-25 00:19:58 +0900

    feat: 기본 계산기능 추가

--- calculator/src/components/Calculator.js
+++ calculator/src/components/Calculator.js
@@ -1,25 +1,52 @@
+import React, { useState } from 'react';
+
 function Calculator() {
+  const [value, setValue] = useState('');
+
+  const onClick = (event) => {
+    const buttonText = event.target.innerText;
+    if (buttonText === 'C') {
+      setValue('');
+    } else if (buttonText === '=') {
+      try {
+        const result = eval(value);
+        console.log('result!');
+        setValue(result.toString());
+      } catch (error) {
+        alert('Invalid input!');
+        setValue('');
+      }
+    } else {
+      setValue(value + buttonText);
+    }
+  }
+
   return (
-    <div className="calculator">
-      <input type="text" />
-      <div className="button-grid">
-        <button>C</button>
-        <button>/</button>
-        <button>*</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="equals-button">=</button>
-        <button>0</button>
-        <button>.</button>
+    <div className="container">
+      <div className="calculator">
+        <div>
+          <input type="text" value={value} />
+        </div>
+        <div className="button-grid">
+          <button onClick={onClick}>C</button>
+          <button onClick={onClick}>/</button>
+          <button onClick={onClick}>*</button>
+          <button onClick={onClick}>^</button>
+          <button onClick={onClick}>7</button>
+          <button onClick={onClick}>8</button>
+          <button onClick={onClick}>9</button>
+          <button onClick={onClick}>-</button>
+          <button onClick={onClick}>4</button>
+          <button onClick={onClick}>5</button>
+          <button onClick={onClick}>6</button>
+          <button onClick={onClick}>+</button>
+          <button onClick={onClick}>1</button>
+          <button onClick={onClick}>2</button>
+          <button onClick={onClick}>3</button>
+          <button classname="equals-button" onClick={onClick}>=</button>
+          <button onClick={onClick}>0</button>
+          <button onClick={onClick}>.</button>
+        </div>
       </div>
     </div>
   )
