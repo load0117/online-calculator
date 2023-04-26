23edba772da0ac7a3660432eeb5e1e9022a06a4e
HK <load0117@gmail.com>
2023-04-27 00:52:28 +0900

    fix: 2뎁스로 변경

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,16 +1,16 @@
 import Header from "./components/Header";
-import Calculator from "./components/Calculator";
+import Calculator from "./components/model/Calculator";
 import Footer from "./components/Footer";
 import './assets/App.css';
 
 function App() {
-    return (
-        <div className="center">
-            <Header />
-            <Calculator />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="center">
+      <Header />
+      <Calculator />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
